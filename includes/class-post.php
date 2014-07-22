d99e109d867dcde14c31815744a1e81ae11cfbab
3TKY <bastiaantk@hotmail.com>
2014-07-22 23:16:15 +0200

    Map out post class functions, create time ago string function for posts

--- includes/class-post.php
+++ includes/class-post.php
@@ -1,7 +1,49 @@
 <?php
 class Post {
 	private $dbh;
-	private $config; 
+	private $config;
+
+	public function create() {
+		
+	}
+
+	public function display() {
+		
+	}
+
+	public function delete() {
+		
+	}
+
+	public function hide() {
+		
+	}
+
+	public function edit() {
+		
+	}
+
+	public function getTimeAgoString($t) {
+		$units = array("s", "m", "h", "d", "w", "m", "y");
+		$unit_length = array("60","60","24","7","4.35","12");
+
+		//Get time elapsed since timestamp
+		$elapsed = time() - $t;
+
+		//Get appropriate unit
+		for ($unit = 0; $elapsed >= $unit_length[$unit] && $unit < count($unit_length)-1; $unit++) {
+			$elapsed /= $unit_length[$unit];
+		}
+
+		$elapsed = round($elapsed);
+
+		//Return elapsed time string
+		if ($unit > 2) {
+			return 'about ' . $elapsed . $units[$unit] . ' ago';
+		} else {
+			return $elapsed . $units[$unit] . ' ago';
+		}
+	}
 
 	public function __construct(Config $config, PDO $dbh) {
 		$this->config = $config;
