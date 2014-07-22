aa332ea1124f12d436a4ff7f32427e38352a0618
3TKY <bastiaantk@hotmail.com>
2014-07-22 22:36:58 +0200

    Set dependencies for user and post class

--- includes/class-post.php
+++ includes/class-post.php
@@ -1,5 +1,18 @@
 <?php
 class Post {
+	private $dbh;
+	private $config;
+	private $name = NULL;
+	private $password = NULL;
 	
+	$stmt = $dbh->prepare("SELECT name FROM users WHERE id = :user_id");
+	$stmt->bindParam(':user_id', $id, PDO::PARAM_INT);
+	$stmt->execute();
+	$result = $stmt->fetch(); 
+
+	public function __construct(Config $config, Database $dbh) {
+		$this->config = $config;
+		$this->dbh = $dbh;
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -3,10 +3,13 @@ session_start();
 
 require_once('includes/class-config.php');
 require_once('includes/class-db.php');
+require_once('includes/class-post.php');
 
 $config = new Config;
 $db = new Database($config);
 $dbh = $db->connect();
+$post = new Post($config, $dbh);
+$user = new User($config, $dbh);
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $config->getLanguage(); ?>">
