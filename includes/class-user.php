ed944d5e2c79a9ad526edab783ad01dcfe1bb859
3TKY <bastiaantk@hotmail.com>
2014-07-31 19:47:21 +0200

    Add register function validation

--- includes/class-user.php
+++ includes/class-user.php
@@ -8,66 +8,51 @@ class User {
 	public $email;
 	private $user_id;
 	private $last_login;
+	private $password_hashed;
 
 	private $logged_in;
 
 	public function register() {
+		//Error array
+		$e = [];
 		$user_exists = $this->userExists($this->name, $this->email);
 
-		print_r($user_exists);
-
+		//Check if unique index user data already exists
 		if (!$user_exists['name'] && !$user_exists['email']) {
-			/*
-			$stmt = $this->dbh->prepare("INSERT INTO users (name, password, email) VALUES (:name, :password, :email)");
-			$stmt->bindParam(':name', $this->name);
-			$stmt->bindParam(':password', $this->password);
-			$stmt->bindParam(':email', $this->email);
-			$stmt->execute();
-			*/
-
-			$email_valid = filter_var($this->email, FILTER_VALIDATE_EMAIL);
-			$name_valid = ;
-
-
-
+			$input_valid = $this->validateUser($this->name, $this->email, $this->password);
+			
+			//Validate register form input
+			if($input_valid['name'] && $input_valid['email'] && $input_valid['password']) {
+				/*
+				$stmt = $this->dbh->prepare("INSERT INTO users (name, password, email) VALUES (:name, :password, :email)");
+				$stmt->bindParam(':name', $this->name);
+				$stmt->bindParam(':password', $this->password);
+				$stmt->bindParam(':email', $this->email);
+				$stmt->execute();
+				*/
+				echo 'user valid';
+			} else {
+				if (!$input_valid['name']) {
+					//Invalid name error
+				}
+				if (!$input_valid['email']) {
+					//Invalid email error
+				}
+				if (!$input_valid['password']) {
+					//Invalid password error
+				}
+			}
 		} else {
 			if ($user_exists['name']) {
-			
+				//Name already exists error
 			}
 			if ($user_exists['email']) {
-			
+				//Email already exists error
 			}
 		}
-
-		/*$stmt = $this->dbh->prepare("INSERT INTO users (name, password, email) VALUES (:name, :password, :email)");
-		$stmt->bindParam(':name', $this->name);
-		$stmt->bindParam(':password', $this->password);
-		$stmt->bindParam(':email', $this->email);
-		$stmt->execute();*/
-	}
-
-	public function userExists($name = NULL, $email = NULL) {
-		$name_exists;
-		$email_exists;
-
-		if ($name) {
-			$stmt = $this->dbh->prepare("SELECT COUNT(*) AS num FROM users WHERE name = :name");
-			$stmt->bindParam(':name', $this->name);
-			$stmt->execute();
-			$result = $stmt->fetch(PDO::FETCH_ASSOC);
-			$name_exists = $result['num'];
-		}
-
-		if ($email) {
-			$stmt = $this->dbh->prepare("SELECT COUNT(*) AS num FROM users WHERE email = :email");
-			$stmt->bindParam(':email', $this->email);
-			$stmt->execute();
-			$result = $stmt->fetch(PDO::FETCH_ASSOC);
-			$email_exists = $result['num'];
-		}
-
-		$user_exists = array('name' => $name_exists, 'email' => $email_exists);
-		return $user_exists;
+		
+		//Output JSON response
+		return $response = 'JSON';
 	}
 
 	public function login() {
@@ -84,10 +69,12 @@ class User {
 		$this->logged_in = 0;
 	}
 
+	//Delete a user
 	public function delete($user_id) {
 		
 	}
 
+	//Ban user for violation of rules
 	public function ban($reason, $duration) {
 		
 	}
@@ -103,13 +90,29 @@ class User {
 		}
 	}
 
+	//Validate user data input
+	private function validateUser($name, $email, $password) {
+		$email_valid = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+		$name_valid = preg_match('/^[a-zA-Z0-9_]{1,16}$/', $name);
+
+		$password_valid = preg_match('/^(?=.*[A-Z].*[A-Z])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z]).{6,}$/', $password);
+
+		$input_valid = array('name' => $name_valid, 'email' => $email_valid, 'password' => $password_valid);
+
+		return $input_valid;
+	}
+
+	//Get name from user id
 	public function getName() {
 		if ($this->user_id) {
 			$stmt = $this->dbh->prepare("SELECT name FROM users WHERE id = :user_id");
 			$stmt->bindParam(':user_id', $this->user_id, PDO::PARAM_INT);
 			$stmt->execute();
 			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
 			$this->name = $result['name'];
+
 			return $this->name;
 		}
 	}
@@ -120,9 +123,38 @@ class User {
 			$stmt->bindParam(':user_id', $this->user_id, PDO::PARAM_INT);
 			$stmt->execute();
 			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
 			$this->last_login = $result['last_login'];
+
 			return $this->last_login;
 		}
+	}
+
+	public function userExists($name = NULL, $email = NULL) {
+		$name_exists = NULL;
+		$email_exists = NULL;
+
+		if ($name) {
+			$stmt = $this->dbh->prepare("SELECT COUNT(*) AS num FROM users WHERE name = :name");
+			$stmt->bindParam(':name', $this->name);
+			$stmt->execute();
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			$name_exists = $result['num'];
+		}
+
+		if ($email) {
+			$stmt = $this->dbh->prepare("SELECT COUNT(*) AS num FROM users WHERE email = :email");
+			$stmt->bindParam(':email', $this->email);
+			$stmt->execute();
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			$email_exists = $result['num'];
+		}
+
+		$user_exists = array('name' => $name_exists, 'email' => $email_exists);
+
+		return $user_exists;
 	}
 
 	public function __construct(Config $config, PDO $dbh, $user_id = NULL) {
--- register.php
+++ register.php
@@ -1,8 +1,4 @@
 <?php
-$name = $_GET['name'];
-$pass = $_GET['pass'];
-$email = $_GET['email'];
-
 require_once('includes/class-config.php');
 require_once('includes/class-db.php');
 require_once('includes/class-user.php');
@@ -11,10 +7,16 @@ $config = new Config;
 $db = new Database($config);
 $dbh = $db->connect();
 
+//Temporary register form
+$name = $_GET['name'];
+$pass = $_GET['pass'];
+$email = $_GET['email'];
+
+//Create new instance of user for the user to be registered and assign input values
 $user = new User($config, $dbh);
 $user->name = $name;
 $user->password = $pass;
 $user->email = $email;
 
-$user->register();
+echo $user->register();
 ?>
